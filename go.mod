a59874b235317e20804015001af508812f4e2a20
gerardo.zuniga <gerardo.zuniga@globant.com>
2023-06-07 12:07:57 -0600

    Second commit

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module account-managment-get-http-body-request
 
 go 1.19
+
+require (
+	github.com/fatih/color v1.14.1 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/uptrace/bunrouter v1.0.20 // indirect
+	github.com/uptrace/bunrouter/extra/reqlog v1.0.20 // indirect
+	go.opentelemetry.io/otel v1.13.0 // indirect
+	go.opentelemetry.io/otel/trace v1.13.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,90 @@
 package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	bun "github.com/uptrace/bunrouter"
+	"github.com/uptrace/bunrouter/extra/reqlog"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	ContentTypeHeaderKey                  = "Content-Type"
+	ContentTypeApplicationJsonHeaderValue = "application/json"
+	V1BaseEndpoint                        = "/v1"
+	UserPathGroup                         = "/users"
+	ServerPort                            = "8080"
+)
+
+func main() {
+	router := bun.New(bun.Use(reqlog.NewMiddleware()))
+	setAPIRoutes(router)
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", ServerPort),
+		Handler: router,
+	}
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Println("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		fmt.Println("Server Shutdown:", err)
+	}
+	<-ctx.Done()
+}
+
+func decodeSaveTransactionRequest(body io.ReadCloser) (fuwdn *FindUserWithDisplayNameCommand, apiEr error) {
+	err := json.NewDecoder(body).Decode(&fuwdn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return
+}
+
+type FindUserWithDisplayNameCommand struct {
+	DisplayName string
+}
+
+func setAPIRoutes(router *bun.Router) {
+	// Transaction group middleware
+	v1Group := router.NewGroup(V1BaseEndpoint).Use(func(next bun.HandlerFunc) bun.HandlerFunc {
+		return func(w http.ResponseWriter, req bun.Request) error {
+			w.Header().Add(ContentTypeHeaderKey, ContentTypeApplicationJsonHeaderValue)
+			return next(w, req)
+		}
+	})
+
+	// group for 'users' routes
+	v1Group.WithGroup(UserPathGroup, func(g *bun.Group) {
+		// mutes/bans user from chat
+		g.GET("/:id", HandleGetByDisplayName)
+	})
+
+}
+
+func HandleGetByDisplayName(w http.ResponseWriter, req bun.Request) error {
+	defer req.Body.Close()
+	reqFindUserWithDisplayNameCommand, err := decodeSaveTransactionRequest(req.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println(*reqFindUserWithDisplayNameCommand)
+	return nil
+}
